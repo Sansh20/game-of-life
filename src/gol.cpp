43d6e6a72a573dd53de5122d7c89b3d9978f2d32
Sanyam Sharma <sanyamsh20@gmail.com>
2022-05-03 11:33:40 +0530

    Implemented the logic of gol

--- src/gol.cpp
+++ src/gol.cpp
@@ -4,6 +4,37 @@
 int gol::run(sf::RenderWindow &window) {
 
     sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+
+    sf::Texture playTexture, pauseTexture;
+    sf::Sprite play, pause;
+
+    if(!playTexture.loadFromFile("./res/pngs/play.png")){
+        std::cout<<"Unable to load Play Texture";
+    }
+    
+    if(!pauseTexture.loadFromFile("./res/pngs/pause.png")){
+        std::cout<<"Unable to load Pause Texture";
+    }
+
+    sf::Text info;
+    sf::Font rWM;
+
+    if(!rWM.loadFromFile("./res/fonts/Raleway-Medium.ttf")){
+        std::cout<<"Unable to load the Raleway Medium";
+    }
+
+    info.setFont(rWM);
+    info.setString("K - Pause, R - Reset");
+    info.setPosition(40, 6);
+    info.setFillColor(sf::Color::Black);
+    info.setOutlineColor(sf::Color(169, 169, 169, 255));
+    info.setOutlineThickness(0.5);
+
+    play.setTexture(playTexture);
+    pause.setTexture(pauseTexture);
+
+    play.setPosition(10, 10);
+    pause.setPosition(10, 10);
     
     float blockSize= 17;
     bool isPaused = true;
@@ -14,6 +45,8 @@ int gol::run(sf::RenderWindow &window) {
     int grid[nRows][nCols];
 
     memset(grid, 0, sizeof(grid));
+
+    int newGrid[nRows][nCols];
 
     while(window.isOpen()){
         sf::Event event;
@@ -31,12 +64,22 @@ int gol::run(sf::RenderWindow &window) {
                         int x = (mousePos.x / blockSize);
                         int y = (mousePos.y / blockSize);
                         grid[x][y] = !grid[x][y];
-                    }          
+                    }  
+                    break;        
 
                 case sf::Event::KeyPressed:
                     if(event.key.code == sf::Keyboard::K){
                         isPaused = !isPaused;
                     }
+                    else if(event.key.code == sf::Keyboard::R){
+                        for(int i=0; i<nRows; i++){
+                            for(int j=0; j<nCols; j++){
+                                grid[i][j] = 0;
+                            }
+                        }
+                        isPaused = true;
+                    }
+                    break;
 
                 default:
                     break;
@@ -61,6 +104,7 @@ int gol::run(sf::RenderWindow &window) {
 
                 window.draw(block);
                 
+
                 // neighbourCount+=grid[i-1][j-1];
                 // neighbourCount+=grid[i][j-1];
                 // neighbourCount+=grid[i+1][j-1];
@@ -69,11 +113,48 @@ int gol::run(sf::RenderWindow &window) {
                 // neighbourCount+=grid[i-1][j+1];
                 // neighbourCount+=grid[i][j+1];
                 // neighbourCount+=grid[i+1][j+1];
+                if(!isPaused){
+
+                    newGrid[i][j] = grid[i][j];
+
+                    for(int x=-1; x<2; x++){
+                        for(int y=-1; y<2; y++){
+                            neighbourCount+=grid[i+x][j+y];
+                        }
+                    }
+
+                    neighbourCount -= grid[i][j];
+                    
+                    if(grid[i][j]==1 && (neighbourCount>3 || neighbourCount<2)){
+                        newGrid[i][j] = 0;
+                    }
+                    else if(neighbourCount==3){
+                        newGrid[i][j] = 1;
+                    }
+                }
 
             }
         }
 
+        if(!isPaused){
+            for(int i=0; i<nRows; i++){
+                for(int j=0; j<nCols; j++){
+                    grid[i][j] = newGrid[i][j];
+                }
+            }
+            window.draw(pause);
+        }
+        else{
+            window.draw(play);
+        }
+
+        window.draw(info);
+
         window.display();
+
+        if(!isPaused){
+            sf::sleep(sf::milliseconds(100));
+        }
     }
 
     return -1;
